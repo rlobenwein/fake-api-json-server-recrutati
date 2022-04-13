fa95576000b0cf1b2ea0694d9ef01b9e8aff9db7
Rodrigo Lobenwein <62033829+rlobenwein@users.noreply.github.com>
2022-04-12 22:12:31 -0300

    Initial Commit

---
+++ recrutatidb.js
@@ -0,0 +1,21 @@
+const recrutadores = require("./data/recrutadores");
+const usuarios = require("./data/usuarios");
+const projetos = require("./data/projetos");
+const certificados = require("./data/certificados");
+const linguagens = require("./data/linguagens");
+const linguagens_usuarios = require("./data/linguagens_usuarios");
+const idiomas = require("./data/idiomas");
+const idiomas_usuarios = require("./data/idiomas_usuarios");
+const mensagens = require("./data/mensagens");
+
+module.exports = {
+    recrutadores,
+    usuarios,
+    projetos,
+    certificados,
+    linguagens,
+    linguagens_usuarios,
+    idiomas,
+    idiomas_usuarios,
+    mensagens,
+}
