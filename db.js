d1a16ba3fb27fef3c11c42bdacafe610b40b86ad
Rodrigo Lobenwein <62033829+rlobenwein@users.noreply.github.com>
2022-04-25 21:54:15 -0300

    Last

--- db.js
+++ db.js
@@ -19,28 +19,3 @@ module.exports = {
   idiomasUsuarios,
   mensagens
 }
-
-
-// const items = require("./data/items");
-
-// module.exports = () => {
-
-//   var tags = items.map((i) => i.tags);
-//   tags = [].concat.apply([], tags);
-//   tags = [...new Set(tags)];
-
- 
-
-//   var brands = items.map((i) => i.manufacturer);
-//   brands = [...new Set(brands)];
-
-//   var itemTypes = items.map((i) => i.itemType);
-//   itemTypes = [...new Set(itemTypes)];
-
-//   return {
-//     items,
-//     tags,
-//     brands,
-//     itemTypes,
-//   };
-// };
